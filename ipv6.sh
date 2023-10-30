2ef1da3358d5a68ad48b758d3743ab290c6c08d5
maksqi <maks164836@gmail.com>
2023-10-30 14:15:22 +0100

    .

--- ipv6.sh
+++ ipv6.sh
@@ -40,14 +40,13 @@ users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n" \
-"auth strong\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $6 " -i" $3 " -e"$7"\n" \
+"proxy -n -a -p" $4 " -i" $3 "\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
+
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
